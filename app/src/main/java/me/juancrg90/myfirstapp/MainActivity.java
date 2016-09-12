d77038ed31290a4ce3071a703a90446513cf76d0
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2016-09-12 09:06:24 -0500

    Add SendMessage Event

--- app/src/main/java/me/juancrg90/myfirstapp/MainActivity.java
+++ app/src/main/java/me/juancrg90/myfirstapp/MainActivity.java
@@ -2,6 +2,8 @@ package me.juancrg90.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void sendMessage(View view) {
+
+        Log.v("MENSAJE DE LOG", "NO DISTRAIGAN A SUS COMPAÑEROS!!!!!");
+
+    }
+
+
 }
