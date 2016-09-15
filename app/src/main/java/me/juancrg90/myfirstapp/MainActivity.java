881a06d219fd594caf6d17933ca6460d38d96f4b
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2016-09-15 08:25:17 -0500

    Add event Listener click

--- app/src/main/java/me/juancrg90/myfirstapp/MainActivity.java
+++ app/src/main/java/me/juancrg90/myfirstapp/MainActivity.java
@@ -4,18 +4,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSend;
+    EditText message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSend = (Button)findViewById(R.id.buttonSend);
+        message = (EditText) findViewById(R.id.editText);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessage(view);
+            }
+        });
+
+
     }
 
     public void sendMessage(View view) {
 
-        Log.v("MENSAJE DE LOG", "NO DISTRAIGAN A SUS COMPAÑEROS!!!!!");
+        Log.v("ESCRIBI:", message.getText().toString());
 
     }
 
